01605b74088647ab4359d179d5fa12d0d3d0d1bc
Ashutosh Jha <atulashu38@gmail.com>
2026-04-08 11:34:52 +0000

    string to number conversin

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -18,6 +18,22 @@ let booleanIsLoggedIn = Boolean(isLoggedIn)
 // ""=> false
 // "Ashu" => true
 let someNumber = 33
-let stringNumber = String(someNumber)
- console.log(typeof stringNumber);
- 
+let stringNumber = String(someNumber) /*
+ console.log(typeof stringNumber); */
+
+// **************************Operations*********************************
+let value = 3
+let negValue = -value /*
+console.log(negValue); */
+
+//console.log(2+2) addition
+//console.log(2-2) subtraction
+//console.log(2*2) multiply
+//console.log(2**3) power
+//console.log(2/2) division
+//console.log(2%2) modulus
+
+let strl1= "Hello"
+let strl2 = "Ashutosh"
+let strl3 = strl1 +  strl2
+console.log(strl3);

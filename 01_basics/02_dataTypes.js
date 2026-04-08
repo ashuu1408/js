60182f5d752f8b46306b90e644f53d07eda9d930
Ashutosh Jha <atulashu38@gmail.com>
2026-04-08 09:43:51 +0000

    datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,23 @@
+"use strict"; // treat all js codeas newer version
+
+//alert(3+3) we are using nodejs, not browser
+
+console.log(3+3)
+//code readibility should be high
+console.log("Ashu")
+
+let name = "Ashu" //string
+let age = 18 //number
+let isLoggedIn = false //boolean
+
+// number => 2 to the power 53 (range)
+//bigint
+// string => ""
+//boolean => true/false
+// null => standalone value
+// undefined  => 
+// symbol => unique
+//object
+
+console.log(typeof undefined); //output => Undefined
+console.log(typeof null); //output => Object (VVI Interview que)

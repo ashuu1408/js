82ab894dd3a17adbde9fa4fe8de9cbd31abb82fc
Ashutosh Jha <atulashu38@gmail.com>
2026-04-08 11:07:10 +0000

    conversion of data types

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,23 @@
+let score = "False"
+//nsole.log(typeof score)
+
+let valueInNumber = Number(score)
+//ole.log(typeof valueInNumber);
+//nsole.log(valueInNumber);
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1; false => 0
+
+let isLoggedIn = "Ashu"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+//nsole.log(booleanIsLoggedIn);
+
+// 1 => true ; 0 => false
+// ""=> false
+// "Ashu" => true
+let someNumber = 33
+let stringNumber = String(someNumber)
+ console.log(typeof stringNumber);
+ 

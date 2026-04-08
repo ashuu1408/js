5d5695a58b426f9dc31ad0c3f21df0f5d2468fab
Ashutosh Jha <atulashu38@gmail.com>
2026-04-08 09:48:07 +0000

    variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,22 @@
+const accountId = 144553
+let accountEmail = "atulashu38@gmail.com"
+var accountPassword = "12345" //not prefer to use
+accountCity = "Jaipur"
+
+//accountId = 2 //not allowed 
+
+accountEmail= "hjbwe@gmail.com"
+accountPassword = "21212121"
+accountCity = "Benguluru"
+let accountState;
+
+console.log(accountId);
+
+/* 
+Prefer not to use var 
+because of issue in block scope and functional scope
+*/
+
+
+console.table([accountId , accountEmail ,accountPassword, accountCity,accountState ]);
+

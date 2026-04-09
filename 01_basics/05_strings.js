f742caad90382520739464a6ac9ff65737a5885d
Ashutosh Jha <atulashu38@gmail.com>
2026-04-09 19:02:36 +0000

    string

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,7 @@
+const name = "Ashutosh"
+const repoCount = 4
+
+// console.log (name + repoCount + " Value");  old way of writing syntax
+
+console.log(`Hello my name is ${name} and my repo 
+    count is ${repoCount}`);

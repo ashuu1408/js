3568fd55b2b95bf25492713bec3f8c293187a12f
Ashutosh Jha <atulashu38@gmail.com>
2026-04-10 18:53:56 +0000

    string

--- 01_basics/05_strings.js
+++ 01_basics/05_strings.js
@@ -5,3 +5,33 @@ const repoCount = 4
 
 console.log(`Hello my name is ${name} and my repo 
     count is ${repoCount}`);
+
+const gameName = new String('Ashutosh-arya')
+
+console.log(gameName[0]);
+console.log(gameName.__proto__)
+
+console.log(gameName.length)
+console.log(gameName.toUpperCase())
+console.log(gameName.charAt(2));
+console.log(gameName.indexOf('t'))
+
+const newString = gameName.substring(0, 4)
+console.log(newString)
+
+const anotherString = gameName.slice(-7, 4)
+console.log(anotherString);
+
+const newStringOne = "    Ashutosh    "
+console.log(newStringOne)
+console.log(newStringOne.trim())
+
+const url = "https://ashuu.in/Ashutosh%20Jha"
+
+console.log(url.replace('%20', '-'))
+
+
+console.log(url.includes('Arya'))
+
+console.log(gameName.split('-'));
+

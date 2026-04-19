fecd86a09c112674a44da086df9b30db3177bb35
Ashutosh Jha <atulashu38@gmail.com>
2026-04-19 04:12:25 +0000

    math and num

--- 01_basics/06nums_and_maths.js
+++ 01_basics/06nums_and_maths.js
@@ -7,3 +7,31 @@ console.log(balance)
 
 console.log(balance.toString().length);
 console.log(balance.toFixed(2));
+
+ const otherNumber = 123.89666
+
+ console.log(otherNumber.toPrecision(3));
+
+ const hundreds = 1000000
+ console.log(hundreds.toLocaleString('en-IN'));
+ 
+ //**********************Maths***************************** 
+
+ console.log(Math);
+ console.log(Math.abs(-4));
+ console.log(Math.round(4.6845));
+ console.log(Math.ceil(547.567));
+ console.log(Math.floor(5.578));
+ 
+
+
+ console.log(Math.random()); //gives value between 0 and 1
+ console.log((Math.random()*10) + 1); // *by 10 because we are shifting values in left and adding 1 because we need value more than 1
+ console.log(Math.floor(Math.random()*10) + 1);
+ 
+ const min = 10
+ const max = 20
+
+ console.log(Math.floor(Math.random() * (max - min + 1)) + min)
+ 
+ 
